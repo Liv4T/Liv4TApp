b12ea52a1b0c1984e73b230649352f6464b9bca5
Daniel Buitrago <dbuitragosa01@gmail.com>
2019-10-09 16:05:25 -0500

    Oct 09 - version 1.5.1 alpha. Se corrigen bugs en la pantalla de registro

--- app/src/main/java/com/dybcatering/live4teach/Login/RegisterActivity.java
+++ app/src/main/java/com/dybcatering/live4teach/Login/RegisterActivity.java
@@ -1,25 +1,54 @@
 package com.dybcatering.live4teach.Login;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.dybcatering.live4teach.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
-    private EditText name, email, password, c_password;
+    private EditText nombre, apellido, email, usuario, password, c_password, telefono;
     private Button btn_regist;
     private ProgressBar loading;
     private static String URL_REGIST = "http://192.168.1.18:8080/Androidlogin/register.php";
 
+    public Spinner spinnerRegistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        spinnerRegistro = findViewById(R.id.spinnerregistro);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.spinner, android.R.layout.simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerRegistro.setAdapter(arrayAdapter);
+        spinnerRegistro.setOnItemSelectedListener(this);
+        nombre = findViewById(R.id.txtNombre);
+        apellido= findViewById(R.id.txtApellido);
+        email = findViewById(R.id.txtMail);
+        usuario = findViewById(R.id.txtUsuario);
+        password = findViewById(R.id.txtPassword);
+        c_password = findViewById(R.id.txtPasswordConfirm);
+        telefono = findViewById(R.id.txtTelefono);
+        btn_regist = findViewById(R.id.btn_registrarse);
 
+        nombre.setEnabled(false);
+        apellido.setEnabled(false);
+        email.setEnabled(false);
+        usuario.setEnabled(false);
+        password.setEnabled(false);
+        c_password.setEnabled(false);
+        telefono.setEnabled(false);
+        btn_regist.setEnabled(false);
         // loading = findViewById(R.id.loading);
        // name = findViewById(R.id.name);
        // email = findViewById(R.id.email);
@@ -92,4 +121,49 @@ public class RegisterActivity extends AppCompatActivity {
     }*/
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text =  parent.getItemAtPosition(position).toString();
+       if (text.equals("Tutor")){
+
+           String url = "http://www.dybcatering.com";
+           Intent i = new Intent(Intent.ACTION_VIEW);
+           i.setData(Uri.parse(url));
+           startActivity(i);
+           nombre.setEnabled(false);
+           apellido.setEnabled(false);
+           email.setEnabled(false);
+           usuario.setEnabled(false);
+           password.setEnabled(false);
+           c_password.setEnabled(false);
+           telefono.setEnabled(false);
+           btn_regist.setEnabled(false);
+
+        } else if (text.equals("Estudiante")) {
+           nombre.setEnabled(true);
+           apellido.setEnabled(true);
+           email.setEnabled(true);
+           usuario.setEnabled(true);
+           password.setEnabled(true);
+           c_password.setEnabled(true);
+           telefono.setEnabled(true);
+           btn_regist.setEnabled(true);
+        } else if (text.equals("Seleccionar...")){
+           nombre.setEnabled(false);
+           apellido.setEnabled(false);
+           email.setEnabled(false);
+           usuario.setEnabled(false);
+           password.setEnabled(false);
+           c_password.setEnabled(false);
+           telefono.setEnabled(false);
+           btn_regist.setEnabled(false);
+
+       }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
