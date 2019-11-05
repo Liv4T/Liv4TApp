8e116c94273e4ac0fee093c047081d71e66e83f8
Daniel Buitrago <dbuitragosa01@gmail.com>
2019-11-05 16:33:58 -0500

    Nov 05 - versión 2.0.0alpha, se agrega funcionalidad de subfragments, y adicional se implementa un nuevo widget para youtube que tiene mejor soporte y no es tan inestable, posible eliminación de YoutubeConfig, se inicia con la funcionalidad de subfragmentos en las demas pantallas

--- app/src/main/java/com/dybcatering/live4teach/Estudiante/MisCursos/MisCursosDetalleFragment.java
+++ app/src/main/java/com/dybcatering/live4teach/Estudiante/MisCursos/MisCursosDetalleFragment.java
@@ -7,16 +7,56 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.dybcatering.live4teach.R;
+import com.pierfrancescosoffritti.youtubeplayer.player.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayer;
+import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayerInitListener;
+import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayerView;
+
+import java.util.List;
 
 public class MisCursosDetalleFragment extends Fragment {
 
+	YouTubePlayer youTubePlayer;
+	LottieAnimationView scrolldebajo;
+	View view;
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_mis_cursos_detalle, container, false);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		View view = inflater.inflate (R.layout.fragment_mis_cursos_detalle, container, false);
+
+		scrolldebajo = view.findViewById(R.id.buttoniniciar);
+
+		scrolldebajo.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getActivity(), "hola", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+
+	YouTubePlayerView youTubePlayerView = view.findViewById(R.id.youtube_player_view);
+
+		youTubePlayerView.initialize(new YouTubePlayerInitListener() {
+			@Override
+			public void onInitSuccess(final YouTubePlayer initializedYouTubePlayer) {
+				initializedYouTubePlayer.addListener(new AbstractYouTubePlayerListener() {
+					@Override
+					public void onReady() {
+						initializedYouTubePlayer.loadVideo("v9oyNBBXDi8", 0);
+					}
+
+				});
+			}
+		}, true);
+
+
+
+		return view;
 	}
 
 }
