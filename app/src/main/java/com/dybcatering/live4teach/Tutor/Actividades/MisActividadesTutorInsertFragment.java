c18f783bf8db9f5011739c38593c4994ef5c838e
Daniel Buitrago <master@digitalandroidservices.com>
2020-02-13 16:55:10 -0500

    Feb 13 - versión 22.5.0 Se realiza el cargue del spinner desde MisActividadesTutorInsertFragment, listo para realizar el insert

--- app/src/main/java/com/dybcatering/live4teach/Tutor/Actividades/MisActividadesTutorInsertFragment.java
+++ app/src/main/java/com/dybcatering/live4teach/Tutor/Actividades/MisActividadesTutorInsertFragment.java
@@ -5,23 +5,137 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.dybcatering.live4teach.Login.SessionManager;
 import com.dybcatering.live4teach.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MisActividadesTutorInsertFragment extends Fragment {
 
-	public MisActividadesTutorInsertFragment() {
-		// Required empty public constructor
-	}
-
-
+	private static final String TAG = MisActividadesTutorInsertFragment.class.getSimpleName(); //getting the info
+	private static String URL_READ = "https://dybcatering.com/back_live_app/miscursos/detail_course.php";
+	SessionManager sessionManager;
+	String id_usuario;
+	private Spinner SpinnerCurso, SpinnerUnidad, SpinnerActividad;
+	ArrayList<String> Curso;
+	ArrayList<String> Unidad;
+	private RequestQueue mRequestQueue;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
-
-				View view = inflater.inflate(R.layout.fragment_mis_actividades_tutor_insert, container, false);
+		View view = inflater.inflate(R.layout.fragment_mis_actividades_tutor_insert, container, false);
 		// Inflate the layout for this fragment
+		Curso = new ArrayList<>();
+		Unidad = new ArrayList<>();
+		sessionManager = new SessionManager(getActivity());
+		HashMap<String, String> user = sessionManager.getUserDetail();
+		id_usuario = user.get(SessionManager.ID);
+		SpinnerCurso = view.findViewById(R.id.spcurso);
+		SpinnerUnidad = view.findViewById(R.id.spunidad);
+		listarCurso(id_usuario);
+
 		return view;
+	}
+
+	private void listarCurso(final String id){
+		String URL_CARGAR = "https://dybcatering.com/back_live_app/miscursos/misactividades/tutor/listarcursos.php";
+		RequestQueue requestQueue=Volley.newRequestQueue(getActivity().getApplicationContext());
+		StringRequest stringRequest=new StringRequest(Request.Method.POST, URL_CARGAR, new Response.Listener<String>() {
+			@Override
+			public void onResponse(String response) {
+				try{
+					JSONObject jsonObject=new JSONObject(response);
+					JSONArray jsonArray=jsonObject.getJSONArray("Registros");
+					for(int i=0;i<jsonArray.length();i++){
+						JSONObject jsonObject1=jsonArray.getJSONObject(i);
+						String curso=jsonObject1.getString("name");
+						Curso.add(curso);
+					}
+					SpinnerCurso.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, Curso));
+
+				}catch (JSONException e){
+					e.printStackTrace();
+					Toast.makeText(getContext(), "algo salio mal" + e, Toast.LENGTH_SHORT).show();}
+			}
+		}, new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				error.printStackTrace();
+				Toast.makeText(getActivity(), "algo salio mal" + error, Toast.LENGTH_SHORT).show();
+			}
+		}) {
+			@Override
+			protected Map<String, String> getParams() {
+				Map<String, String> params = new HashMap<>();
+				params.put("id", id);
+				return params;
+			}
+		};
+		int socketTimeout = 30000;
+		RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+		stringRequest.setRetryPolicy(policy);
+		requestQueue.add(stringRequest);
+//		final String CursoUnidad = SpinnerCurso.getSelectedItem().toString();
+
+		//listarUnidad(CursoUnidad);
+	}
+
+	private void listarUnidad(final String CursoUnidad){
+		String URL_CARGAR = "https://dybcatering.com/back_live_app/miscursos/misactividades/tutor/listarunidad.php";
+		RequestQueue requestQueue=Volley.newRequestQueue(getActivity().getApplicationContext());
+		StringRequest stringRequest=new StringRequest(Request.Method.POST, URL_CARGAR, new Response.Listener<String>() {
+			@Override
+			public void onResponse(String response) {
+				try{
+					JSONObject jsonObject=new JSONObject(response);
+					JSONArray jsonArray=jsonObject.getJSONArray("Registros");
+					for(int i=0;i<jsonArray.length();i++){
+						JSONObject jsonObject1=jsonArray.getJSONObject(i);
+						String name=jsonObject1.getString("name");
+						Unidad.add(name);
+					}
+					SpinnerUnidad.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, Unidad));
+
+				}catch (JSONException e){
+					e.printStackTrace();
+					Toast.makeText(getContext(), "algo salio mal" + e, Toast.LENGTH_SHORT).show();}
+			}
+		}, new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				error.printStackTrace();
+				Toast.makeText(getActivity(), "algo salio mal" + error, Toast.LENGTH_SHORT).show();
+			}
+		}) {
+			@Override
+			protected Map<String, String> getParams() {
+				Map<String, String> params = new HashMap<>();
+				params.put("name", CursoUnidad);
+				return params;
+			}
+		};
+		int socketTimeout = 30000;
+		RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+		stringRequest.setRetryPolicy(policy);
+		requestQueue.add(stringRequest);
 	}
 
 }
